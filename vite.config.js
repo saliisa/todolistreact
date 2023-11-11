fc65e5a7e03e5e73d58c9cc9361cdf8498f94575
Isabella <Isabella.salim@edu.omnia.fi>
2023-11-11 16:24:21 +0200

    Updated commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todolistreact/', // add this property and the value is the name of your repository
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  } 
 })
